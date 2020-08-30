eb906e110bda7b144b7b7e7b6ae016b62afde209
dinuka thilakarathne <ashandc4@gmail.com>
2020-08-30 14:35:43 +0530

    added color assets

---
+++ DinukaThilakarathne-COBSCCOMP191P-047/UI/Buttons/RoundedButton/RoundedButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButton.swift
+//  DinukaThilakarathne-COBSCCOMP191P-047
+//
+//  Created by Dinuka Thilakarathne on 8/30/20.
+//  Copyright © 2020 Dinuka Thilakarathne. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
