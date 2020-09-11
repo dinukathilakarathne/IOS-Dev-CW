b288ae659e7336ede9a541487744615a78fe5c0b
dinuka thilakarathne <ashandc4@gmail.com>
2020-09-11 13:34:59 +0530

    added question widget

---
+++ DinukaThilakarathne-COBSCCOMP191P-047/UI/SurveyWidgets/Question/Question.swift
@@ -0,0 +1,36 @@
+//
+//  Question.swift
+//  DinukaThilakarathne-COBSCCOMP191P-047
+//
+//  Created by Dinuka Thilakarathne on 9/11/20.
+//  Copyright © 2020 Dinuka Thilakarathne. All rights reserved.
+//
+
+import UIKit
+
+class Question: UIView {
+
+    @IBOutlet var contentView: UIView!
+    
+    @IBOutlet weak var radioButton: RadioButton!
+    
+    @IBOutlet weak var question: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    func configure() {
+        Bundle.main.loadNibNamed("Question", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+
+}
